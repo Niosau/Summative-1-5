4bc3505a045abf62ba6285fe8ddcd2980f92500b
Cameron Link <362704579@GoTVDSB.ca>
2026-04-29 13:16:02 -0400

    Dragon

--- Summative 1-5/Game1.cs
+++ Summative 1-5/Game1.cs
@@ -22,7 +22,12 @@ namespace Summative_1_5
         Rectangle bunnyRect, window, playButton;
         Vector2 bunnySpeed;
         SoundEffect bonk;
+        Texture2D dragonFly, dragonFire;
+        Rectangle dragonRect;
+        Vector2 dragonSpeed;
+        bool fly = true;
 
+        
         // A timer that stores milliseconds.
         float bunnyFrameTimer;
         int totalAnimations = 8; // change this when you add more
@@ -57,6 +62,10 @@ namespace Summative_1_5
             playButton = new Rectangle(300, 200, 100, 100);
             bunnyRect = new Rectangle(100, 100, 100, 100);
             bunnySpeed = new Vector2(1, 0);
+            dragonRect = new Rectangle(800, 100, 100, 100);
+            dragonSpeed = new Vector2(0, 0);
+
+
             _graphics.PreferredBackBufferWidth = window.Width;  // set this value to the desired width of your window
             _graphics.PreferredBackBufferHeight = window.Height;   // set this value to the desired height of your window
             bunnyStartTimer = 0;
@@ -68,18 +77,27 @@ namespace Summative_1_5
             bunnyFrameTimer = 0;
 
             // You can change this to alter the speed of the animation (lower number = faster animation).
-            threshold = 250;
+            threshold = 100;
 
             // Three sourceRectangles contain the coordinates of Alex's three down-facing sprites on the charaset.
             bunnyRectangles = new Rectangle[8];
+            //BACKWARDS   bunnyRectangles[7] = new Rectangle(0, 0, 369, 313);
+            //bunnyRectangles[6] = new Rectangle(372, 0, 311, 347);
+            //bunnyRectangles[5] = new Rectangle(720, 0, 277, 369);
+            //bunnyRectangles[4] = new Rectangle(0, 360, 363, 320);
+            //bunnyRectangles[3] = new Rectangle(365, 360, 362, 317);
+            //bunnyRectangles[2] = new Rectangle(726, 360, 340, 335);
+            //bunnyRectangles[1] = new Rectangle(0, 720, 308, 365);
+            //bunnyRectangles[0] = new Rectangle(315, 720, 335, 360);
+
             bunnyRectangles[0] = new Rectangle(0, 0, 369, 313);
             bunnyRectangles[1] = new Rectangle(372, 0, 311, 347);
             bunnyRectangles[2] = new Rectangle(720, 0, 277, 369);
-            bunnyRectangles[3] = new Rectangle(0, 315, 363, 320);
-            bunnyRectangles[4] = new Rectangle(365, 350, 362, 317);
-            bunnyRectangles[5] = new Rectangle(726, 372, 340, 335);
-            bunnyRectangles[6] = new Rectangle(0, 325, 309, 370);
-            bunnyRectangles[7] = new Rectangle(315, 325, 318, 357);
+            bunnyRectangles[3] = new Rectangle(0, 360, 363, 320);
+            bunnyRectangles[4] = new Rectangle(365, 360, 362, 317);
+            bunnyRectangles[5] = new Rectangle(726, 360, 340, 335);
+            bunnyRectangles[6] = new Rectangle(0, 720, 308, 365);
+            bunnyRectangles[7] = new Rectangle(315, 720, 330, 360);
 
             // This tells the animation to start on the left-side sprite.
             previousAnimationIndex = 2;
@@ -89,6 +107,13 @@ namespace Summative_1_5
             playButtonImg = Content.Load<Texture2D>("playButton");
             bnnuy = Content.Load<Texture2D>("LayDumbRabbit");
             charaset = Content.Load<Texture2D>("BunnyRoll45");
+            if (fly == true) {
+                dragonFly = Content.Load<Texture2D>("dragonFly");
+            }
+            if(fly == false) {
+                dragonFly = Content.Load<Texture2D>("dragonFireBreathe");
+            }
+                
             // TODO: use this.Content to load your game content here
         }
 
@@ -111,13 +136,18 @@ namespace Summative_1_5
             else if (screen == Screen.Animation)
             {
                 // TODO: Add your update logic here
-                
 
+                dragonRect.X += (int)dragonSpeed.X;
+                dragonRect.Y += (int)dragonSpeed.Y;
                 bunnyRect.X += (int)bunnySpeed.X;
                 UpdateBunnyFrame(gameTime);
+                if (dragonRect.X == 680)
+                {
+                    dragonSpeed = new Vector2(0, 0); 
+                        fly = false;
+                }
 
-                
-               
+
             }
             // TODO: Add your update logic here
 
@@ -138,8 +168,13 @@ namespace Summative_1_5
             {
                 // Only draw the area contained within the sourceRectangle.
                 _spriteBatch.Draw(charaset, bunnyRect, bunnyRectangles[currentAnimationIndex], Color.White);
-
+                if (bunnyRect.X == 570)
+                {
+                    _spriteBatch.Draw(dragonFly, dragonRect, Color.White);
+                    
+                }
             }
+
             _spriteBatch.End();
 
             base.Draw(gameTime);
@@ -157,6 +192,16 @@ namespace Summative_1_5
                 }
                 bunnyFrameTimer = 0;
             }
+            if (bunnyRect.X == 570)
+            {
+                bunnySpeed = new Vector2(0, 0);
+                threshold = 0;
+                currentAnimationIndex = 0;
+                dragonSpeed = new Vector2(-1, 0);
+
+
+            }
+           
         }
 
     }
