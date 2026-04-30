bc8767bca1ce76bcf64a4ddbeb28d0c5b4f5cc20
Cameron Link <362704579@GoTVDSB.ca>
2026-04-30 12:12:42 -0400

    Test

--- Summative 1-5/Game1.cs
+++ Summative 1-5/Game1.cs
@@ -18,12 +18,10 @@ namespace Summative_1_5
         private SpriteBatch _spriteBatch;
         Screen screen;
         MouseState mouseState, prevMouseState;
-        Texture2D titleScreen, bnnuy, playButtonImg, charaset, dragonFly, dragoneFire;
+        Texture2D titleScreen, bnnuy, playButtonImg, charaset, dragonFly, dragonFire, dragonTexture;
         Rectangle bunnyRect, window, playButton, dragonRect;
         Vector2 bunnySpeed;
         SoundEffect bonk;
-        Texture2D dragonFly, dragonFire;
-        Rectangle dragonRect;
         Vector2 dragonSpeed;
         bool fly = true;
 
@@ -57,9 +55,9 @@ namespace Summative_1_5
             screen = Screen.Title;
             mouseState = Mouse.GetState();
             base.Initialize();
-            window = new Rectangle(0, 0, 1250, 1250);
+            window = new Rectangle(0, 0, 800, 250);
             //384, 182
-            playButton = new Rectangle(300, 200, 100, 100);
+            playButton = new Rectangle(400, 125, 100, 100);
             bunnyRect = new Rectangle(100, 100, 100, 100);
             bunnySpeed = new Vector2(1, 0);
             dragonRect = new Rectangle(800, 100, 100, 100);
@@ -69,6 +67,7 @@ namespace Summative_1_5
             _graphics.PreferredBackBufferWidth = window.Width;  // set this value to the desired width of your window
             _graphics.PreferredBackBufferHeight = window.Height;   // set this value to the desired height of your window
             bunnyStartTimer = 0;
+            _graphics.ApplyChanges();
             base.Initialize();
         }
 
@@ -93,11 +92,11 @@ namespace Summative_1_5
             bunnyRectangles[0] = new Rectangle(0, 0, 369, 313);
             bunnyRectangles[1] = new Rectangle(372, 0, 311, 347);
             bunnyRectangles[2] = new Rectangle(720, 0, 277, 369);
-            bunnyRectangles[3] = new Rectangle(0, 315, 363, 320);
-            bunnyRectangles[4] = new Rectangle(365, 350, 362, 317);
-            bunnyRectangles[5] = new Rectangle(726, 372, 340, 335);
-            bunnyRectangles[6] = new Rectangle(0, 325, 309, 370);
-            bunnyRectangles[7] = new Rectangle(315, 325, 318, 357);
+            bunnyRectangles[3] = new Rectangle(0, 360, 363, 320);
+            bunnyRectangles[4] = new Rectangle(365, 360, 362, 317);
+            bunnyRectangles[5] = new Rectangle(726, 360, 340, 335);
+            bunnyRectangles[6] = new Rectangle(0, 720, 308, 365);
+            bunnyRectangles[7] = new Rectangle(315, 720, 335, 360);
 
             // This tells the animation to start on the left-side sprite.
             previousAnimationIndex = 2;
@@ -107,13 +106,13 @@ namespace Summative_1_5
             playButtonImg = Content.Load<Texture2D>("playButton");
             bnnuy = Content.Load<Texture2D>("LayDumbRabbit");
             charaset = Content.Load<Texture2D>("BunnyRoll45");
-            if (fly == true) {
-                dragonFly = Content.Load<Texture2D>("dragonFly");
-            }
-            if(fly == false) {
-                dragonFly = Content.Load<Texture2D>("dragonFireBreathe");
-            }
+            
+            dragonFly = Content.Load<Texture2D>("dragonFly"); 
+            dragonFire = Content.Load<Texture2D>("dragonFireBreathe");
+            dragonTexture = dragonFly;
                 
+
+
             // TODO: use this.Content to load your game content here
         }
 
@@ -141,10 +140,26 @@ namespace Summative_1_5
                 dragonRect.Y += (int)dragonSpeed.Y;
                 bunnyRect.X += (int)bunnySpeed.X;
                 UpdateBunnyFrame(gameTime);
+
+                if (bunnyRect.X == 570)
+                {
+                    bunnySpeed = new Vector2(0, 0);
+                    threshold = 0;
+                    currentAnimationIndex = 0;
+                    dragonSpeed = new Vector2(-1, 0);
+                }
+
+
                 if (dragonRect.X == 680)
                 {
                     dragonSpeed = new Vector2(0, 0); 
-                        fly = false;
+                    dragonTexture = dragonFire;
+                    bunnySpeed.X = -1;
+                }
+
+                if (bunnyRect.X == window.X)
+                {
+                    bunnySpeed.X = 0;
                 }
 
 
@@ -168,9 +183,9 @@ namespace Summative_1_5
             {
                 // Only draw the area contained within the sourceRectangle.
                 _spriteBatch.Draw(charaset, bunnyRect, bunnyRectangles[currentAnimationIndex], Color.White);
-                if (bunnyRect.X == 570)
+                if (bunnyRect.X == 570 || fly == false) 
                 {
-                    _spriteBatch.Draw(dragonFly, dragonRect, Color.White);
+                    _spriteBatch.Draw(dragonTexture, dragonRect, Color.White);
                     
                 }
             }
@@ -183,24 +198,39 @@ namespace Summative_1_5
         public void UpdateBunnyFrame(GameTime gameTime)
         {
             bunnyFrameTimer += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
-            if (bunnyFrameTimer > threshold)
+
+
+
+            if(bunnySpeed.X > 0)
             {
-                currentAnimationIndex++;
-                if (currentAnimationIndex >= totalAnimations)
+                if (bunnyFrameTimer > threshold)
                 {
-                    currentAnimationIndex = 0;
+                    currentAnimationIndex++;
+                    if (currentAnimationIndex >= totalAnimations)
+                    {
+                        currentAnimationIndex = 0;
+                    }
+                    bunnyFrameTimer = 0;
                 }
-                bunnyFrameTimer = 0;
+                
             }
-            if (bunnyRect.X == 570)
+            else if (bunnySpeed.X < 0)
             {
-                bunnySpeed = new Vector2(0, 0);
-                threshold = 0;
-                currentAnimationIndex = 0;
-                dragonSpeed = new Vector2(-1, 0);
-
-
+                threshold = 100;
+                if (bunnyFrameTimer > threshold)
+                {
+                    
+                    currentAnimationIndex--;
+                    if (currentAnimationIndex >= totalAnimations)
+                    {
+                        currentAnimationIndex = 7;
+                        fly = false;
+                    }
+                    bunnyFrameTimer = 0;
+                    
+                }
             }
+            
            
         }
 
